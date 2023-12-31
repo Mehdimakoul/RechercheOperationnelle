3cb5b9f4cd099ca026a4b59a9b21e276028a773c
makoul_mehdi <elmehdi.elmakoul@etu.univ-tours.fr>
2023-12-31 23:58:35 +0100

    Update NearestNeighbor.java

--- src/main/java/com/alexscode/teaching/tap/NearestNeighbor.java
+++ src/main/java/com/alexscode/teaching/tap/NearestNeighbor.java
@@ -1,5 +1,4 @@
 package com.alexscode.teaching.tap;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,28 +7,35 @@ public class NearestNeighbor implements TAPSolver {
 
     @Override
     public List<Integer> solve(Instance ist) {
+        // Initialisation des objectifs et de la solution
         Objectives obj = new Objectives(ist);
         List<Integer> solution = new ArrayList<>();
 
+        // Initialisation de la liste des nœuds non visités
         List<Integer> unvisitedNodes = new ArrayList<>();
         for (int i = 0; i < ist.size; i++)
             unvisitedNodes.add(i);
 
+        // Sélection d'un nœud initial de manière aléatoire
         int currentNode = (int) (Math.random() * ist.size);
         solution.add(currentNode);
         unvisitedNodes.remove(Integer.valueOf(currentNode));
 
+        // Boucle principale de l'algorithme
         while (!unvisitedNodes.isEmpty() &&
                 (obj.time(solution) < ist.timeBudget && obj.distance(solution) < ist.maxDistance)) {
+            // Recherche du nœud le plus proche parmi les nœuds non visités
             int nearestNode = findNearestNode(currentNode, unvisitedNodes, ist);
             solution.add(nearestNode);
             unvisitedNodes.remove(Integer.valueOf(nearestNode));
             currentNode = nearestNode;
         }
 
+        // Retourne la solution en enlevant le dernier nœud (redondant)
         return solution.subList(0, solution.size() - 1);
     }
 
+    // Recherche du nœud le plus proche parmi les nœuds non visités
     private int findNearestNode(int currentNode, List<Integer> unvisitedNodes, Instance ist) {
         int nearestNode = -1;
         double nearestDistance = Double.MAX_VALUE;
